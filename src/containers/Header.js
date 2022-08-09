c073f5d9e900f1f0e13cfea61f2bad82f844a3d3
Sourav Pahwa <Sourav61pahwa@gmail.com>
2022-08-09 14:27:29 +0530

    added header code
    added header code

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,11 +1,38 @@
 import React from "react";
+import "../App.css";
 
 const Header = () => {
   return (
-    <div className="ui fixed-menu">
-      <div className="ui container center">
-        <h1>Fake Shop</h1>
-      </div>
+    <div class="wrapper">
+      <nav>
+        <input type="checkbox" id="show-search" />
+        <div class="content">
+          <div class="logo">
+            <a href="#">FakeShop</a>
+          </div>
+          <ul class="links">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </div>
+        <label for="show-search" class="search-icon">
+          <i class="fas fa-search"></i>
+        </label>
+        <form action="#" class="search-box">
+          <input
+            type="text"
+            placeholder="Type Something to Search..."
+            required
+          />
+          <button type="submit" class="go-icon">
+            <i class="fas fa-long-arrow-alt-right"></i>
+          </button>
+        </form>
+      </nav>
     </div>
   );
 };
